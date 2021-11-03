a00cc09f015814db2a85e3d402fd317553d57f26
Greg Ziegan <greg.ziegan@gmail.com>
2021-11-02 22:23:36 -0500

    remove log

--- pages/src/Page/Admin/DetainerWarrants/Edit.elm
+++ pages/src/Page/Admin/DetainerWarrants/Edit.elm
@@ -3641,7 +3641,7 @@ updateDetainerWarrant domain maybeCred form =
                  , ( "defendants", Encode.list encodeRelated form.defendants )
                  , ( "amount_claimed_category", Encode.string (DetainerWarrant.amountClaimedCategoryText form.amountClaimedCategory) )
                  ]
-                    ++ nullable "file_date" Time.Utils.posixEncoder (Debug.log "fileDate" form.fileDate)
+                    ++ nullable "file_date" Time.Utils.posixEncoder form.fileDate
                     ++ nullable "status" Encode.string (Maybe.map DetainerWarrant.statusText form.status)
                     ++ nullable "plaintiff" encodeRelated form.plaintiff
                     ++ nullable "plaintiff_attorney" encodeRelated form.plaintiffAttorney
