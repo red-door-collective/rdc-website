19063a30fcbb30ad5eac778462033e541266a5b0
Greg Ziegan <greg.ziegan@gmail.com>
2021-11-12 12:37:05 -0600

    scrape for individual dw documents

--- eviction_tracker/detainer_warrants/scrape.py
+++ eviction_tracker/detainer_warrants/scrape.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from selenium.common.exceptions import ElementNotInteractableException
 from selenium.webdriver.support.wait import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import os
+import re
+import time
 
 caselink_username = os.environ['CASELINK_USERNAME']
 caselink_password = os.environ['CASELINK_PASSWORD']
@@ -14,8 +17,8 @@ browser = webdriver.Chrome()
 try:
     browser.get('https://caselink.nashville.gov')
     browser.switch_to.frame("update")
-    # browser.find_element(By.ID, "OPERCODE").send_keys(caselink_username)
-    username_field = WebDriverWait(browser, 3).until(
+
+    username_field = WebDriverWait(browser, 5).until(
         EC.presence_of_element_located((By.ID, 'OPERCODE'))
     )
 
@@ -27,25 +30,43 @@ try:
     docket_search = WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.XPATH, '//input[@name="P_21"]'))
     )
-    browser.implicitly_wait(2)
-    docket_search.send_keys('21GT3993')
+    # browser.implicitly_wait(2)
+    try:
+        docket_search.send_keys('21GT3993')
+    except ElementNotInteractableException:
+        time.sleep(.5)
+        docket_search.send_keys('21GT3993')
 
     browser.find_element(By.XPATH, '//button[@name="WTKCB_20"]').click()
 
-    dw = WebDriverWait(browser, 5).until(
-        EC.presence_of_element_located((By.ID, 'L_TRN_3'))
-    )
+    # dw = WebDriverWait(browser, 5).until(
+    #     EC.presence_of_element_located((By.ID, 'L_TRN_3'))
+    # )
 
+    # WebDriverWait(browser, 5).until(
+    #     EC.frame_to_be_available_and_switch_to_it('postback')
+    # )
+
+    print('made it to postback')
+
+    time.sleep(3)
     browser.switch_to.frame("postback")
 
-    script_text = browser.get_element(
-        '/html/head/script[2]').get_attribute('outerHTML')
+    script_tag = browser.find_element(
+        By.XPATH, "/html")
+    # script_tag = WebDriverWait(browser, 5).until(
+    #     EC.presence_of_element_located(
+    #         (By.XPATH, "//*[contains(text(),'function PostRead()')]"))
+    # )
+    postback_HTML = script_tag.get_attribute('outerHTML')
+    print(postback_HTML)
 
     documents_regex = re.compile(
         r'\,\s*"ý(https://caselinkimages.nashville.gov.+?)ý+"\,')
 
-    urls_mess = documents_regex.search(script_text).group(1)
+    urls_mess = documents_regex.search(postback_HTML).group(1)
     urls = [url for url in urls_mess.split('ý') if url != '']
+    print(urls)
 
     # documents = browser.find_elements(
     #     By.XPATH, '//*[@id="GRIDTBL_1A"]/tbody/tr/td[3]/input')
