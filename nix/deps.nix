7222107c14768efd5e896a9ce353d608f59c2e8b
Greg Ziegan <greg.ziegan@gmail.com>
2022-01-14 16:04:14 -0600

    bump dependencies

--- nix/deps.nix
+++ nix/deps.nix
@@ -23,21 +23,20 @@ let
         }
       );
 
-       cryptography = super.cryptography.overridePythonAttrs(old:{
-          cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
-            inherit (old) src;
-            name = "${old.pname}-${old.version}";
-            sourceRoot = "${old.pname}-${old.version}/src/rust/";
-            sha256 = "sha256-tQoQfo+TAoqAea86YFxyj/LNQCiViu5ij/3wj7ZnYLI=";
-          };
-          cargoRoot = "src/rust";
-          nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs.rustPlatform; [
-            rust.rustc
-            rust.cargo
-            cargoSetupHook
-          ]);
-        });
-
+      cryptography = super.cryptography.overridePythonAttrs(old:{
+        cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
+          inherit (old) src;
+          name = "${old.pname}-${old.version}";
+          sourceRoot = "${old.pname}-${old.version}/src/rust/";
+          sha256 = "sha256-kozYXkqt1Wpqyo9GYCwN08J+zV92ZWFJY/f+rulxmeQ=";
+        };
+        cargoRoot = "src/rust";
+        nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs.rustPlatform; [
+          rust.rustc
+          rust.cargo
+          cargoSetupHook
+        ]);
+      });
   });
 
 in rec {
