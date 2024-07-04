48b441c13300653cf66f72d0aba01d7265616c6b
Greg Ziegan <greg.ziegan@gmail.com>
2024-07-04 11:57:52 -0500

    rename google var

--- rdc_website/jobs.py
+++ rdc_website/jobs.py
@@ -89,7 +89,7 @@ def export():
         logger.info(f"Exporting upcoming court data to Google Sheets")
 
         workbook_name = "Website Export"
-        key = scheduler.app.config["GOOGLE_ACCOUNT_PATH"]
+        key = scheduler.app.config["GOOGLE_SERVICE_ACCOUNT"]
         logger.info(f"Exporting upcoming court dates to workbook: {workbook_name}")
         detainer_warrants.exports.to_court_watch_sheet(workbook_name, key)
         courtroom_entry_wb = f'{datetime.strftime(date.today(), "%B %Y")} Court Watch'
