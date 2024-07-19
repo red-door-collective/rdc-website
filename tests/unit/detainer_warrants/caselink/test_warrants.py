26842348f835737c3b7c20b44edf0c41cca00ea6
Greg Ziegan <greg.ziegan@gmail.com>
2024-07-18 21:23:37 -0500

    fix unit tests

--- tests/unit/detainer_warrants/caselink/test_warrants.py
+++ tests/unit/detainer_warrants/caselink/test_warrants.py
@@ -125,21 +125,6 @@ class TestWarrants(RDCTestCase):
             ],
         )
 
-    def test_extract_pleading_document_paths(self):
-        pleading_documents = None
-        with open("tests/fixtures/caselink/case-page/pleading-documents.html") as f:
-            pleading_documents = f.read()
-
-        paths = warrants.extract_pleading_document_paths(pleading_documents)
-
-        self.assertEqual(
-            paths,
-            [
-                "\\Public\\Sessions\\24\\24GT4890\\3370253.pdf",
-                "\\Public\\Sessions\\24/24GT4890\\03370254.pdf",
-            ],
-        )
-
 
 if __name__ == "__main__":
     unittest.main()
