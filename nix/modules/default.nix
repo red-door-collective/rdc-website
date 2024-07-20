bb8d01e24482e0b0050736f6bd358839e15e69bf
Greg Ziegan <greg.ziegan@gmail.com>
2024-07-19 19:16:57 -0500

    source it i guess

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -33,7 +33,7 @@ with builtins; let
   '';
 
   rdcWebsiteConsole = pkgs.writeScriptBin "rdc-website-console" ''
-    export `${rdcShowEnvVars}`
+    export `${source /rdcShowEnvVars}`
     ${serveApp}/bin/console
   '';
 
